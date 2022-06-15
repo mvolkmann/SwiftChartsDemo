d484d14c18ee7dc9de5021e918d38343782f0b9a
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-06-14 19:53:17 -0500

    Initial Commit

---
+++ SwiftChartsDemo/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftChartsDemo
+//
+//  Created by Mark Volkmann on 6/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftChartsDemo/SwiftChartsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsDemoApp.swift
+//  SwiftChartsDemo
+//
+//  Created by Mark Volkmann on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
