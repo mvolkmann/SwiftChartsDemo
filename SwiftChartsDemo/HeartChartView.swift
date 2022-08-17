10435da270444e1ba60606651368a4b01a431465
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-08-16 19:35:05 -0500

    progress

--- SwiftChartsDemo/HeartChartView.swift
+++ SwiftChartsDemo/HeartChartView.swift
@@ -1,32 +1,60 @@
 import Charts
+import HealthKit
 import SwiftUI
 
 struct HeartChartView: View {
+    @State private var frequency: String = "Day"
+    @State private var identifier: HKQuantityTypeIdentifier = .heartRate
+    @State private var timespan: String = "1 Week"
     @StateObject var vm = HealthKitViewModel.shared
 
     var body: some View {
         VStack {
             Text("Heart Rate (\(vm.heartRate.count))")
                 .fontWeight(.bold)
-
+            picker(
+                label: "Span",
+                values: ["1 Day", "1 Week", "1 Month", "3 Months"],
+                selected: $timespan
+            )
+            picker(
+                label: "Frequency",
+                values: ["Minute", "Hour", "Day"],
+                selected: $frequency
+            )
+            HStack {
+                Text("Metric").fontWeight(.bold)
+                Picker("", selection: $identifier) {
+                    ForEach(Metrics.shared.sorted) {
+                        Text($0.name).tag($0.identifier)
+                    }
+                }
+                Spacer()
+            }
             Chart(vm.heartRate) { heart in
                 LineMark(
                     x: .value("Date", heart.date),
                     y: .value("BPM", heart.value)
                 )
             }
-
-            /*
-            List {
-                ForEach(vm.heartRate) { heart in
-                    Text("\(heart.date) \(heart.ms) \(heart.value)")
-                }
-            }
-            */
         }
         .padding()
         .task {
             await HealthKitViewModel.shared.load()
+        }
+    }
+
+    private func picker(
+        label: String,
+        values: [String],
+        selected: Binding<String>
+    ) -> some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Text(label).fontWeight(.bold)
+            Picker("", selection: selected) {
+                ForEach(values, id: \.self) { Text($0) }
+            }
+            .pickerStyle(.segmented)
         }
     }
 }
--- SwiftChartsDemo/Models/Metrics.swift
+++ SwiftChartsDemo/Models/Metrics.swift
@@ -4,142 +4,176 @@ enum Frequency {
     case minute, hour, day
 }
 
-struct Metric {
+struct Metric: Identifiable {
+    let name: String
     let identifier: HKQuantityTypeIdentifier
     let unit: HKUnit
     let option: HKStatisticsOptions
     let frequency: Frequency
     let lowerIsBetter: Bool
+
+    var id = UUID()
 }
 
 class Metrics {
+    static let shared = Metrics()
+
     var map: [HKQuantityTypeIdentifier: Metric] = [:]
 
-    static let shared = Metrics()
+    var sorted: [Metric] {
+        map.values.sorted { $0.name < $1.name }
+    }
 
     // This class is a singleton.
     // swiftlint:disable function_body_length
     private init() {
         addMetricSum(
+            name: "Active Energy Burned (calories)",
             identifier: .activeEnergyBurned,
             unit: .largeCalorie(),
             frequency: .hour
         )
         addMetricSum(
+            name: "Exercise Time (minutes)",
             identifier: .appleExerciseTime,
             unit: .minute()
         )
         addMetricSum(
+            name: "Move Time (calories)",
             identifier: .appleMoveTime,
             unit: .largeCalorie()
         )
         addMetricSum(
+            name: "Stand Time (minutes)",
             identifier: .appleStandTime,
             unit: .minute()
         )
         addMetricAverage(
+            name: "Walking Steadiness %",
             identifier: .appleWalkingSteadiness,
             unit: .percent()
         )
         addMetricSum(
+            name: "Resting Energy Burned (calories)",
             identifier: .basalEnergyBurned,
             unit: .largeCalorie(),
             frequency: .hour
         )
         addMetricAverage(
+            name: "Body Fat %",
             identifier: .bodyFatPercentage,
             unit: .percent(),
             lowerIsBetter: true
         )
         addMetricAverage(
+            name: "Weight (pounds)",
             identifier: .bodyMass,
             unit: .pound(),
             lowerIsBetter: true
         )
         addMetricAverage(
+            name: "Body Mass Index (BMI)",
             identifier: .bodyMassIndex,
             unit: .count(),
             lowerIsBetter: true
         )
         addMetricSum(
+            name: "Distance Cycling (miles)",
             identifier: .distanceCycling,
             unit: .mile()
         )
         addMetricSum(
+            name: "Distance Walking & Running (miles)",
             identifier: .distanceWalkingRunning,
             unit: .mile()
         )
         addMetricSum(
+            name: "Distance Wheelchair (miles)",
             identifier: .distanceWheelchair,
             unit: .mile()
         )
         addMetricAverage(
+            name: "Environmental Audio Exposure",
             identifier: .environmentalAudioExposure,
             unit: HKUnit(from: "dBASPL"),
             lowerIsBetter: true
         )
         addMetricSum(
+            name: "Flights Climbed",
             identifier: .flightsClimbed,
             unit: .count()
         )
         addMetricAverage(
+            name: "Headphone Audio Exposure",
             identifier: .headphoneAudioExposure,
             unit: HKUnit(from: "dBASPL"),
             lowerIsBetter: true
         )
         addMetricAverage(
+            name: "Heart Rate (BPM)",
             identifier: .heartRate,
             unit: HKUnit(from: "count/min"),
             frequency: .minute,
             lowerIsBetter: true
         )
         addMetricAverage(
+            name: "Heart Rate Variability",
             identifier: .heartRateVariabilitySDNN,
             unit: HKUnit.secondUnit(with: .milli),
             lowerIsBetter: true
         )
         addMetricAverage(
+            name: "Lean Body Mass (pounds)",
             identifier: .leanBodyMass,
             unit: .pound(),
             lowerIsBetter: true
         )
         addMetricSum(
+            name: "Number of Times Fallen",
             identifier: .numberOfTimesFallen,
             unit: .count(),
             lowerIsBetter: true
         )
         addMetricAverage(
+            name: "Oxygen Saturation %",
             identifier: .oxygenSaturation,
             unit: .percent()
         )
         addMetricSum(
+            name: "Wheelchair Push Count",
             identifier: .pushCount,
             unit: .count(),
             frequency: .hour
         )
         addMetricAverage(
+            name: "Respiratory Rate (breaths per minute)",
             identifier: .respiratoryRate,
             unit: HKUnit(from: "count/min"),
             lowerIsBetter: true
         )
         addMetricAverage(
+            name: "Resting Heart Rate (BPM)",
             identifier: .restingHeartRate,
             unit: HKUnit(from: "count/min"),
             lowerIsBetter: true
         )
         addMetricAverage(
+            name: "Six Minute Walk Test Distance (meters)",
             identifier: .sixMinuteWalkTestDistance,
             unit: .meter()
         )
         addMetricAverage(
+            name: "Stair Ascent Speed (feet per second)",
             identifier: .stairAscentSpeed,
             unit: HKUnit(from: "ft/s")
         )
         addMetricAverage(
+            name: "Stair Descent Speed (feet per second)",
             identifier: .stairDescentSpeed,
             unit: HKUnit(from: "ft/s")
         )
         addMetricSum(
+            name: "Step Count",
             identifier: .stepCount,
             unit: .count(),
             frequency: .hour
@@ -149,43 +183,51 @@ class Metrics {
         let kgMin = HKUnit.gramUnit(with: .kilo).unitMultiplied(by: .minute())
         // This is reported as "Cardio Fitness" in the Apple Health app.
         addMetricAverage(
+            name: "VO2 Max",
             identifier: .vo2Max,
             unit: mL.unitDivided(by: kgMin)
         )
 
         addMetricAverage(
+            name: "Walking Asymmetry %",
             identifier: .walkingAsymmetryPercentage,
             unit: .percent(),
             lowerIsBetter: true
         )
         addMetricAverage(
+            name: "Walking Double Support %",
             identifier: .walkingDoubleSupportPercentage,
             unit: .percent(),
             lowerIsBetter: true
         )
         addMetricAverage(
+            name: "Walking Heart Rate (BPM)",
             identifier: .walkingHeartRateAverage,
             unit: HKUnit(from: "count/min"),
             lowerIsBetter: true
         )
         addMetricAverage(
+            name: "Walking Speed (meters per second)",
             identifier: .walkingSpeed,
             unit: HKUnit(from: "m/s"), // meters per second
             frequency: .hour
         )
         addMetricAverage(
+            name: "Walking Step Length (inches)",
             identifier: .walkingStepLength,
             unit: .inch()
         )
     }
 
     private func addMetricAverage(
+        name: String,
         identifier: HKQuantityTypeIdentifier,
         unit: HKUnit,
         frequency: Frequency = .day,
         lowerIsBetter: Bool = false
     ) {
         map[identifier] = Metric(
+            name: name,
             identifier: identifier,
             unit: unit,
             option: .discreteAverage,
@@ -195,12 +237,14 @@ class Metrics {
     }
 
     private func addMetricSum(
+        name: String,
         identifier: HKQuantityTypeIdentifier,
         unit: HKUnit,
         frequency: Frequency = .day,
         lowerIsBetter: Bool = false
     ) {
         map[identifier] = Metric(
+            name: name,
             identifier: identifier,
             unit: unit,
             option: .cumulativeSum,
